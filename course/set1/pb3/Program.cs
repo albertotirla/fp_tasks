f1471c170c1a7a888af4bd6730600857eb4130b4
Alberto Tirla <albertotirla@gmail.com>
2021-11-18 17:13:01 +0200

    solve pb3

---
+++ course/set1/pb3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            if (n % k == 0)
+            {
+                Console.WriteLine("da");
+            }
+            else
+            {
+                Console.WriteLine("nu");
+            }
+        }
+    }
+}
