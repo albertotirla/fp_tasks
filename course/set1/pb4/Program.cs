6d0155f93e075101a70107079eff804482d0a6bf
Alberto Tirla <albertotirla@gmail.com>
2021-11-18 17:34:55 +0200

    solve pb4

---
+++ course/set1/pb4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pb4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = int.Parse(Console.ReadLine());
+            //pentru ca anul y sa fie un an bisect, trebuie sa fie multiplu de 4. Pentru a preveni exceptii precum 100, y trebuie sa fie multiplu de 4 si nu de 100, sau de 400.
+            if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+            {
+                Console.WriteLine("da");
+            }
+            else
+            {
+                Console.WriteLine("nu");
+            }
+        }
+    }
+}
