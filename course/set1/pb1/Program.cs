789db9959888560d3dcc241a28b075454c847aaa
Alberto Tirla <albertotirla@gmail.com>
2021-11-18 15:23:32 +0200

    initial commit

---
+++ course/set1/pb1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
