21ea0856252fe4eb7d01d38900dfd974cbfd288b
Alberto Tirla <albertotirla@gmail.com>
2021-11-18 17:50:56 +0200

    fix pb5

---
+++ course/set1/pb5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pb5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int cifra = 0;
+            int temp = n;
+            while (temp != 0)
+            {
+                cifra = temp % 10;
+                counter++;
+                if (counter == k)
+                {
+                    Console.WriteLine(cifra);
+                    Environment.Exit(0);
+                }
+                temp /= 10;
+            }
+            Console.WriteLine($"nu s-a gasit cifra a {k}-a in numarul {n}");
+        }
+    }
+}
