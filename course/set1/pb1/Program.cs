c65e11482c151c895c7341012adc98735bfc8b59
Alberto Tirla <albertotirla@gmail.com>
2021-11-18 16:13:36 +0200

    solve problem 1

--- course/set1/pb1/Program.cs
+++ course/set1/pb1/Program.cs
@@ -6,7 +6,26 @@ namespace pb1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            /*
+                pentru ecuatia de gradul 1(ax+b=0), se aplica rationamentul:
+                ax=-b ->
+                x=-b/a.
+        */
+            //cazuri particulare: cand a=0, ecuatia nu are sens. Cand b=0, rezultatul este 0.
+            if (a == 0)
+            {
+                Console.WriteLine("invalid equation");
+                Environment.Exit(-1);
+            }
+            if (b == 0)
+            {
+                Console.WriteLine("0");
+            }
+            //rezolvarea ecuatiei fara cazurile particulare
+            int x = -b / a;
+            Console.WriteLine(x);
         }
     }
 }
