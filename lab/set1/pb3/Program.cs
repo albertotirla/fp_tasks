1f209ad6402b3a1046e8df59d125dbdcf4b0f904
Alberto Tirla <albertotirla@gmail.com>
2021-11-27 22:37:52 +0200

    solve pb3_lab_s1

---
+++ lab/set1/pb3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            int j = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            // suppose the division reminder between ij and k means i*j%k. If i=2, j=3, k=6, the program will print 0
+            Console.WriteLine((i * j) % k);
+        }
+    }
+}
