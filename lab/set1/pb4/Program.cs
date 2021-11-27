1b37aebfca39a66afaa391e4d7a6dcd7bcad30f4
Alberto Tirla <albertotirla@gmail.com>
2021-11-27 22:47:22 +0200

    solve pb4_lab_s1

---
+++ lab/set1/pb4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pb4
+{
+    class Program
+    {
+        static int sum_div(int n)
+        {
+            int s=0;
+            for(int i=1;i<=n/2;i++)
+            {
+                if(n%i==0) s+=i;
+            }
+        return s;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type a number");
+            int n=int.Parse(Console.ReadLine());
+            if(sum_div(n)==n)
+            {
+                Console.WriteLine("yes");
+            }
+        else
+        {
+            Console.WriteLine("no");
+        }
+        }
+    }
+}
