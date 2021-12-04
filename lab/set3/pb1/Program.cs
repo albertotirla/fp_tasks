01e61710a910591e286666ea0119c7b3368ec0a6
Alberto Tirla <albertotirla@gmail.com>
2021-12-04 20:28:12 +0200

    solve pb1_lab_s3

---
+++ lab/set3/pb1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type sequence length");
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.WriteLine($"type term {i}");
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            //in order to do the task, a sort is required
+            Array.Sort(v);
+            for (int i = 0; i < v.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    for (int k = 0; k < j; k++)
+                    {
+                        if (v[i] == v[j] + v[k]) Console.WriteLine(v[i]);
+                    }
+                }
+            }
+        }
+    }
+}
