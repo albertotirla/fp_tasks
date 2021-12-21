47dfa3ab6005f06b5bcfc3253d329a1f0b97efb3
Alberto Tirla <albertotirla@gmail.com>
2021-12-21 21:04:50 +0200

    solve pb9

---
+++ course/set1/pb9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pb9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //presupunem ca numarul este pozitiv, asa ca punem restrictii asupra datelor citite
+            uint n = uint.Parse(Console.ReadLine());
+            //se vor determina toti divizorii proprii ai numarului
+            for (uint i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
