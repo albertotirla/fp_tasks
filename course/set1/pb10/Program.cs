aa29ac497cddb18a9b46531b2db5b5e5cca84d27
Alberto Tirla <albertotirla@gmail.com>
2021-12-21 21:13:18 +0200

    solve pb10

---
+++ course/set1/pb10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pb10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = uint.Parse(Console.ReadLine());
+            for (uint i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine($"{n} nu este prim");
+                    Environment.Exit(0);
+                }
+            }
+            Console.WriteLine($"{n} este prim");
+        }
+    }
+}
