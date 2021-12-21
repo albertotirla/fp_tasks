6dd9149eb009affb4d40a2dd1dd1fccff9163082
Alberto Tirla <albertotirla@gmail.com>
2021-12-21 20:37:10 +0200

    solve pb7

---
+++ course/set1/pb7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pb7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduceti numerele care sa fie inversate");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"inainte de swap: a={a}, b={b}");
+        a=a^b;
+        b=b^a;
+        a=a^b;
+        Console.WriteLine($"dupa swap: a={a}, b={b}");
+        }
+    }
+}
