45465382f1f96ed08b94756ae2ecc96df0367b06
Alberto Tirla <albertotirla@gmail.com>
2021-12-21 20:15:58 +0200

    solve pb6

---
+++ course/set1/pb6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pb6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaram variabilele
+            Console.WriteLine("introduceti valori pentru verificare");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (
+                a + b > c &&
+                a + c > b &&
+                b + c > a
+        )
+            {
+                Console.WriteLine($"{a}, {b} si {c} pot forma un triunghi");
+            }
+            else
+            {
+                Console.WriteLine($"{a}, {b} si {c} nu pot forma un triunghi");
+            }
+        }
+    }
+}
