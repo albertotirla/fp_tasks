9035c6f61f4f2786b319203530ad92b2b418809f
Alberto Tirla <albertotirla@gmail.com>
2021-12-22 15:35:21 +0200

    solve pb3_s2

---
+++ course/set2/pb3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.WriteLine("n=");
+int n=int.Parse(Console.ReadLine());
+            int s=0;//suma
+            int p=1;//produs
+            for(int i=1;i<=n;i++)
+            {
+                s+=i;
+                p*=i;
+            }
+            Console.WriteLine($"suma este {s}, produsul este {p}");
+            //end
+        }
+    }
+}
