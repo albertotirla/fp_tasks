a0b5137b32b597596a62efc225f835698a379127
Alberto Tirla <albertotirla@gmail.com>
2021-12-22 15:58:58 +0200

    solve pb5_s2

---
+++ course/set2/pb5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace pb5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        Console.WriteLine("introduceti lungimea secventei, numar natural diferit de 0");
+            int length = int.Parse(Console.ReadLine());
+            int pos = 0;
+            int cur_num;
+            int counter=0; //acumulator pentru toate elementele ce indeplinesc conditia
+            while (pos < length)
+            {
+                Console.WriteLine($"introduceti termenul {pos + 1}");
+                cur_num = int.Parse(Console.ReadLine());
+                if(cur_num==pos)
+                {
+                    counter++;
+                }
+                pos++;
+            }
+Console.WriteLine($"numarul de elemente egale cu pozitia lor in secventa este {counter}");
+        }
+    }
+}
