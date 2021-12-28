eed2252b470b985c9af4c0bdb9ae9386a63ad0bd
Alberto Tirla <albertotirla@gmail.com>
2021-12-28 14:08:56 +0200

    solve pb7_set2

---
+++ course/set2/pb7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace pb7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduceti lungimea secventei, numar natural diferit de 0");
+            int length = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduceti termenul 1");
+            int cur_num = int.Parse(Console.ReadLine());
+            int pos = 1;//am citit deja un termen, folosit pentru initializarea minimului si a maximului cu prima valoare din sir
+int minim=cur_num, maxim=cur_num;
+            while (pos < length)
+            {
+                Console.WriteLine($"introduceti termenul {pos + 1}");
+                cur_num = int.Parse(Console.ReadLine());
+                if(cur_num<minim)
+                {
+                    minim=cur_num;
+                }
+                if(cur_num>maxim)
+                {
+                    maxim=cur_num;
+                }
+                pos++;
+            }
+            System.Console.WriteLine($"cel mai mic numar din secventa este {minim}, iar cel mai mare termen este {maxim}");
+        }
+    }
+}
