89d61d2439376060c2bb178d54ce7ff16dff1dc1
Alberto Tirla <albertotirla@gmail.com>
2021-12-30 16:28:22 +0200

    solve pb1_s3

---
+++ course/set3/pb1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security;
+
+namespace pb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduceti lungimea vectorului");
+            int n = int.Parse(Console.ReadLine());
+            int[] vec = new int[n];
+            for (int i = 0; i < vec.Length; i++)
+            {
+                Console.WriteLine($"introduceti numarul de pe pozitia {i}");
+                vec[i] = int.Parse(Console.ReadLine());
+            }
+            //done
+        int suma=0;
+        for (int i = 0; i < vec.Length; i++)
+        {
+         suma+=vec[i];
+        }
+        Console.WriteLine($"suma este {suma}");
+        }
+    }
+}
