97a94ed561da70e3c7c317225cdf202c58a74a1f
Alberto Tirla <albertotirla@gmail.com>
2021-12-30 19:00:32 +0200

    solve pb6_s3

---
+++ course/set3/pb6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+Console.WriteLine("introduceti lungimea vectorului");
+int n = int.Parse(Console.ReadLine());
+int[] vec = new int[n];
+Console.WriteLine("k=");
+int k = int.Parse(Console.ReadLine());
+for (int i = 0; i < vec.Length; i++)
+{
+    Console.WriteLine($"introduceti numarul de pe pozitia {i}");
+    vec[i] = int.Parse(Console.ReadLine());
+}
+for (int i = k; i < vec.Length - 1; i++)
+{
+    vec[i] = vec[i + 1];
+}
+//pentru ca incerc sa nu folosesc metode specifice .net, ultimul element va fi ascuns de la afisare, marimea vectorului nu va fi modificata
+n--;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(vec[i]);
+}
---
+++ course/set3/pb7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace pb7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduceti lungimea vectorului");
+            int n = int.Parse(Console.ReadLine());
+            int[] vec = new int[n];
+            for (int i = 0; i < vec.Length; i++)
+            {
+                Console.WriteLine($"introduceti numarul de pe pozitia {i}");
+                vec[i] = int.Parse(Console.ReadLine());
+            }
+            
+            int inceput = 0;
+            int sfarsit = vec.Length - 1;
+            while (inceput < sfarsit)
+            {
+                vec[inceput] ^= vec[sfarsit];
+                vec[sfarsit] ^= vec[inceput];
+                vec[inceput] ^= vec[sfarsit];
+                inceput++;
+                sfarsit--;
+            }
+            for (int i = 0; i < vec.Length; i++)
+            {
+                Console.WriteLine(vec[i]);
+            }
+        }
+    }
+}
